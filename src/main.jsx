0bb9730f06364505144369281f5b18a6b1d8229c
Nicolas <NIKO.ALTERS@GMAIL.COM>
2026-01-06 15:27:26 -0300

    arreglo botn

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' 
-
-// 1. AGREGA ESTA LÍNEA DE BOOTSTRAP AQUÍ:
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import './index.css' // Tus estilos personalizados (el color verde, etc.)
+import './index.css' 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
